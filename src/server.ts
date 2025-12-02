0e4d2cd877ba095c1615962e2b4f37f1168a0b6f
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-12-02 23:05:35 +0600

    implementing sendjson & cleaning up server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,8 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
 import config from "./config";
 import addRoutes, { RouteHandler, routes } from "./helpers/RoutesHandler";
-
-addRoutes("GET", "/", (req, res) => {
-  res.writeHead(200, { "content-type": "application/json" });
-  res.end(
-    JSON.stringify({
-      message: "Hello from node js with typescript...",
-      path: req.url,
-    })
-  );
-});
+import { sendJson } from "./helpers/send.json";
+import "./routes";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
@@ -33,28 +25,6 @@ const server: Server = http.createServer(
         })
       );
     }
-
-    //root route
-    // if (req.url === "/" && req.method === "GET") {
-    //   // res.writeHead(200, { "content-type": "application/json" });
-    //   // res.end(
-    //   //   JSON.stringify({
-    //   //     message: "Hello from node js with typescript...",
-    //   //     path: req.url,
-    //   //   })
-    //   // );
-    // }
-
-    // // health route
-    // if (req.url === "/api" && req.method === "GET") {
-    //   res.writeHead(200, { "content-type": "application/json" });
-    //   res.end(
-    //     JSON.stringify({
-    //       message: "Health status ok",
-    //       path: req.url,
-    //     })
-    //   );
-    // }
 
     // if (req.url === "/api/users" && req.method === "POST") {
     //   // const user = {
